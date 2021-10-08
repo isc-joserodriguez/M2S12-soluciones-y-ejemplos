9dae9092648b1011815fc6b6c20e392e29f4bc3a
José Rodriguez <isc.joserodriguez@gmail.com>
2021-10-07 22:28:10 -0600

    Ejemplo 4 - Resuelto

--- Ejemplo4/main.js
+++ Ejemplo4/main.js
@@ -1,30 +1,33 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+const ciudades = ['Tepic', 'Monterrey', 'Valladolid', 'Guadalajara', 'Mexico', 'Rusia', 'Tijuana'];
+const dataChart = {
+    labels: ciudades,
+    datasets: [{
+        label: '° Celsius',
+        data: [],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
     type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+    data: dataChart,
     options: {
         scales: {
             y: {
@@ -33,3 +36,15 @@ var myChart = new Chart(ctx, {
         }
     }
 });
+
+const getClima = async (cities) => {
+    const weathers = [];
+    for (let i = 0; i < cities.length; i++) {
+        let response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=616629f9acdc3b22b8b09553e632e5da`);
+        weathers.push((response.data.main.temp - 273).toFixed(2))
+    }
+    dataChart.datasets[0].data = weathers;
+    myChart.update();
+}
+
+getClima(ciudades);
