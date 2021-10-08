d91f10e5e1c13fbf8b56063d19e984e5762571c3
José Rodriguez <isc.joserodriguez@gmail.com>
2021-10-07 20:05:34 -0600

    Ejemplo 2 - Resuelto

--- Ejemplo2/main.js
+++ Ejemplo2/main.js
@@ -1,54 +1,12 @@
-const API_URI = 'https://todo-app-bs.herokuapp.com/v1';
+
+import { signupEvent, loginEvent, infoEvent } from './eventListeners/index.js';
+
 const signupForm = document.getElementById('signupForm');
 const loginForm = document.getElementById('loginForm');
 const getInfoButton = document.getElementById('getInfoButton');
 
-signupForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const dataObject = Object.fromEntries(formData);
-    signup(dataObject);
-})
+signupForm.addEventListener('submit', signupEvent);
 
-loginForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const dataObject = Object.fromEntries(formData);
-    login(dataObject);
-});
+loginForm.addEventListener('submit', loginEvent);
 
-getInfoButton.addEventListener('click', e => {
-    getInfo();
-})
-
-
-const signup = async (data) => {
-    try {
-        const respuesta = await axios.post(API_URI + '/user/signup', data);
-        localStorage.setItem('token', respuesta.data.detail.token);
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-const login = async (data) => {
-    try {
-        const respuesta = await axios.post(API_URI + '/user/login', data);
-        localStorage.setItem('token', respuesta.data.detail.token);
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-const getInfo = async () => {
-    try {
-        const respuesta = await axios.get(API_URI + '/user', {
-            headers: {
-                'Authorization': 'Bearer ' + localStorage.getItem('token')
-            }
-        });
-        console.log(respuesta.data.detail)
-    } catch (e) {
-        console.log(e);
-    }
-}
+getInfoButton.addEventListener('click', infoEvent);
