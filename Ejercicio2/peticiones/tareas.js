842b8b99db362124623fb2aa6f3c6a99143c8e2f
José Rodriguez <isc.joserodriguez@gmail.com>
2021-10-07 21:38:54 -0600

    Ejercicio 2 - Resuelto

--- Ejercicio2/peticiones/tareas.js
+++ Ejercicio2/peticiones/tareas.js
@@ -1,14 +1,4 @@
 const API_URI = 'https://todo-app-bs.herokuapp.com/v1';
-
-const tasksList = document.getElementById('tasksList');
-
-tasksList.addEventListener('click', e => {
-    console.log(e.target.tagName)
-    console.log(e.target.textContent)
-    console.log(e.target.parentNode.getAttribute('id'));
-})
-
-
 export const loadTasks = async () => {
     try {
         const respuesta = await axios.get(API_URI + '/task', {
@@ -28,6 +18,37 @@ export const loadTasks = async () => {
         ))
         tasksList.innerHTML = list;
 
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const updateTask = async (id) => {
+    let title = prompt('Nueva tarea');
+    let description = prompt('Nueva descripción');
+    try {
+        const respuesta = await axios.put(API_URI + '/task/' + id, { title, description }, {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        });
+        alert('Tarea actualizada');
+        loadTasks();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+
+export const deleteTask = async (id) => {
+    try {
+        const respuesta = await axios.delete(API_URI + '/task/' + id, {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        });
+        alert('Tarea eliminada');
+        loadTasks();
     } catch (e) {
         console.log(e);
     }
